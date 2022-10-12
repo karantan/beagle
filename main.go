27355d80e1a2dd9ac9205724fe39f2a599e28c1f
Gasper Vozel <gv@niteo.co>
2022-10-12 14:16:16 +0200

    Add hostname info to slack msg
    Also seperate interval and max-running time durations so that we
    can check for long running processes more frequently.

--- main.go
+++ main.go
@@ -16,10 +16,11 @@ import (
 var log = logger.New("beagle")
 
 func main() {
-	var interval time.Duration
+	var interval, maxRunning time.Duration
 	var processFilter, slackChan, cgroup string
 
-	flag.DurationVar(&interval, "interval", 3*time.Minute, "Frequency of checking for running processes. It's also the max allowed running time for reporting it as a long-running process")
+	flag.DurationVar(&interval, "interval", 5*time.Minute, "Frequency of checking for running processes")
+	flag.DurationVar(&maxRunning, "max-running", 20*time.Minute, "Max allowed running time for reporting it as a long-running process")
 	flag.StringVar(&processFilter, "filter", "php-fpm: pool", "Process filter (i.e. '| grep <filter>')")
 	flag.StringVar(&slackChan, "slack", "", "Slack Channel name where the notification will be posted")
 	flag.StringVar(&cgroup, "cgroup", "", "Cgroup TO which long running processes will be moved.")
@@ -33,22 +34,23 @@ func main() {
 	}
 
 	slackHook := os.Getenv("SLACK_NOTIFICATION")
+	hostname, _ := os.Hostname()
 
 	ticker := time.NewTicker(interval)
 	log.Info("Checking for processes every ", interval)
 	for ; true; <-ticker.C {
-		procs := spotter.FindOldProcesses(processFilter, int(interval.Seconds()))
+		procs := spotter.FindOldProcesses(processFilter, int(maxRunning.Seconds()))
 		for _, p := range procs {
 			log.Info(p)
 
 			msg := &slack.WebhookMessage{
-				Text:    fmt.Sprintf("🐶 I spotted PHP-FPM pool for user %s has been running for more than %s", p.Owner, p.Duration.Round(time.Second)),
+				Text:    fmt.Sprintf("🐶 [%s] I spotted PHP-FPM pool for user %s has been running for more than %s", hostname, p.Owner, p.Duration.Round(time.Second)),
 				Channel: slackChan,
 			}
 			if cgroup != "" {
 				isolater.Isolate(p.PID, cgroup)
 				msg = &slack.WebhookMessage{
-					Text:    fmt.Sprintf("🐶 I isolated %d process (%s) because it has been running for more than %s", p.PID, p.Owner, p.Duration.Round(time.Second)),
+					Text:    fmt.Sprintf("🐶 [%s] I isolated %d process (%s) because it has been running for more than %s", hostname, p.PID, p.Owner, p.Duration.Round(time.Second)),
 					Channel: slackChan,
 				}
 			}
